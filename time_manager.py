95be3711c8104765e7357be0c36d8b7b0e9f3417
WELC0ME <zz-crazy-zz@mail.ru>
2021-04-18 18:05:01 +0300

    merge branches

--- time_manager.py
+++ time_manager.py
@@ -10,19 +10,6 @@ class TimeManager:
         # текущее время
         return int(time.time())
 
-    def fold(self, first, second):
-        # сложение времени
-        time1 = self.from_string(first)
-        time2 = self.from_string(second)
-        time1 = datetime.timedelta(days=time1.day, hours=time1.hour,
-                                   minutes=time1.minute)
-        time2 = datetime.timedelta(days=time2.day, hours=time2.hour,
-                                   minutes=time2.minute)
-        time = time1 + time2
-        return str(
-            '00:00:' + str(time.days) + ':' + str(time.hours) + ':' + str(
-                time.minutes))
-
     def get(self, _time):
         return self.now() - _time
 

8da6e7b40bd74084b195ebace405d0e017aa0769
WELC0ME <zz-crazy-zz@mail.ru>
2021-04-20 18:17:46 +0300

    try fix CORS problem

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from time_manager import TimeManager
 
 # все необходимые константы
 
 APP = Flask(__name__)
+cors = CORS(APP, resources={r"/api/*": {"origins": "*"}})
 USER_ID = -1
 
 TIME = TimeManager()

2f1283bbbfb01907365be54484d5885189116c37
WELC0ME <zz-crazy-zz@mail.ru>
2021-04-21 17:30:22 +0300

    production bug fixed

--- windows/productions.py
+++ windows/productions.py
@@ -87,7 +87,7 @@ def start_production():
         })
     db_sess = create_session()
     production = db_sess.query(Production).get(_request['production_id'])
-    print('start_production', production.iod, _request['production_id'])
+    print('start_production', production.id, _request['production_id'])
     print(production.to_dict()['active'])
     if production.to_dict()['active']:
         return jsonify({
